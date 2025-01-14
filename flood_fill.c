60968c018deae32a0b83103093cde6e9755f0736
daniel <daniel-congo2@hotmail.com>
2025-01-14 15:07:47 +0100

    test minilibx, flood_fill en cours

--- flood_fill.c
+++ flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 15:17:46 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/01/13 15:06:27 by daniel149af      ###   ########.fr       */
+/*   Updated: 2025/01/13 15:26:34 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,27 @@ void	flood_fill(char **map, t_point size, t_point player)
 {
 	char	c;
 	t_point	p;
+	char	**tmp;
 
-	c = map[player.y][player.x];
-	map[player.x][player.y] = 'F';
-	if (player.y > 0 && map[player.x][player.y - 1] != '1')
+	tmp = map;
+	c = tmp[player.y][player.x];
+	tmp[player.x][player.y] = 'F';
+	if (player.y > 0 && tmp[player.x][player.y - 1] != 'F')
 	{
-
+		p.x = player.x;
+		p.y = player.y - 1;
+		flood_fill(tmp, size, p);
+	}
+	else if (player.x > 0 && tmp[player.x][player.y] != 'F')
+	{
+		p.x = player.x - 1;
+		p.y = player.y;
+		flood_fill(tmp, size, p);
+	}
+	else if (player.x < ft_strlen(tmp[0]) - 1 && tmp[player.x][player.y] != 'F')
+	{
+		p.x = player.x - 1;
+		p.y = player.y;
+		flood_fill(tmp, size, p);
 	}
 }
